9ed3caa04576ea7b1c7230d02e98fb5174131d19
Viper Code <fathfajhri40@gmail.com>
2022-10-10 00:50:45 +0700

    add handler post for admin edit field

--- handlers/admin.go
+++ handlers/admin.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"kafekoding/models"
 	"kafekoding/utils"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -169,9 +171,11 @@ func AdminDetail(ctx *gin.Context) {
 					http.Error(ctx.Writer, "Not found", http.StatusNotFound)
 					return
 				}
+				defer flash.Delete()
 				ctx.HTML(http.StatusOK, "admin_detail", gin.H{
-					"user": user,
-					"u":    u,
+					"user":  user,
+					"u":     u,
+					"flash": flash,
 				})
 				return
 			}
@@ -185,10 +189,12 @@ func AdminDetail(ctx *gin.Context) {
 
 				users := models.NewUserModel().GetAllUsers()
 
+				defer flash.Delete()
 				ctx.HTML(http.StatusOK, "admin_detail", gin.H{
 					"user":  user,
 					"room":  room,
 					"users": users,
+					"flash": flash,
 				})
 				return
 			}
@@ -197,4 +203,129 @@ func AdminDetail(ctx *gin.Context) {
 			return
 		}
 	}
+}
+
+// AdminPOSTUser handler for post user
+func AdminPOSTUser(ctx *gin.Context) {
+	session := sessions.Default(ctx)
+
+	user := session.Get("user")
+	if user == nil {
+		flash.Set("danger", "Please login first before you accessing this page.")
+		ctx.Redirect(http.StatusFound, "/admin/login")
+		return
+	}
+
+	if !utils.GetSessionValue(user)["isSuperuser"].(bool) || !utils.GetSessionValue(user)["isStaff"].(bool) {
+		flash.Set("danger", "Please login with SuperUser and Staff user.")
+		ctx.Redirect(http.StatusFound, "/admin/login")
+		return
+	}
+
+	id := ctx.PostForm("id")
+	firstName := ctx.PostForm("first_name")
+	lastName := ctx.PostForm("last_name")
+	username := ctx.PostForm("username")
+	email := ctx.PostForm("email")
+	password := ctx.PostForm("password")
+	isSuperuser := ctx.PostForm("is_superuser")
+	isStaff := ctx.PostForm("is_staff")
+	isActive := ctx.PostForm("is_active")
+	// lastLogin := ctx.PostForm("last_login")
+	// dateJoined := ctx.PostForm("date_joined")
+
+	idInt, _ := strconv.Atoi(id)
+	isSuperuserBool, _ := strconv.ParseBool(isSuperuser)
+	isStaffBool, _ := strconv.ParseBool(isStaff)
+	isActiveBool, _ := strconv.ParseBool(isActive)
+
+	u, err := models.NewUserModel().GetUserByID(uint(idInt))
+	if err != nil {
+		return
+	}
+
+	u.FirstName = firstName
+	u.LastName = lastName
+	u.Username = username
+	u.Email = email
+	u.Password = password
+	u.IsSuperuser = isSuperuserBool
+	u.IsActive = isActiveBool
+	u.IsStaff = isStaffBool
+
+	err = models.GetDB.Save(&u).Error
+	if err != nil {
+		http.Error(ctx.Writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	flash.Set("success", "Successfully upgrade user by username: "+username)
+	ctx.Redirect(http.StatusFound, "/admin/detail?table_name=users&field_id="+id)
+}
+
+// AdminPOSTRoom handler for post room
+func AdminPOSTRoom(ctx *gin.Context) {
+	session := sessions.Default(ctx)
+
+	user := session.Get("user")
+	if user == nil {
+		flash.Set("danger", "Please login first before you accessing this page.")
+		ctx.Redirect(http.StatusFound, "/admin/login")
+		return
+	}
+
+	if !utils.GetSessionValue(user)["isSuperuser"].(bool) || !utils.GetSessionValue(user)["isStaff"].(bool) {
+		flash.Set("danger", "Please login with SuperUser and Staff user.")
+		ctx.Redirect(http.StatusFound, "/admin/login")
+		return
+	}
+
+	id := ctx.PostForm("id")
+	name := ctx.PostForm("name")
+	mentorID := ctx.PostForm("mentor_id")
+	members := ctx.PostForm("members")
+	logo, err := ctx.FormFile("logo")
+	if err != nil {
+		http.Error(ctx.Writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+	desc := ctx.PostForm("desc")
+	content := ctx.PostForm("content")
+
+	idInt, _ := strconv.Atoi(id)
+	mentorIDInt, _ := strconv.Atoi(mentorID)
+	membersInt, _ := strconv.Atoi(members)
+	log.Println(membersInt)
+
+	filename := logo.Filename
+
+	u, _ := models.NewUserModel().GetUserByID(uint(membersInt))
+
+	room, err := models.NewRoomModel().GetRoomByID(uint(idInt))
+	if err != nil {
+		http.Error(ctx.Writer, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	room.Name = name
+	room.MentorID = uint(mentorIDInt)
+	room.Members = append(room.Members, &u)
+	room.Logo = "/media/rooms/" + filename
+	room.Desc = desc
+	room.Content = content
+
+	err = models.GetDB.Save(&room).Error
+	if err != nil {
+		http.Error(ctx.Writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = ctx.SaveUploadedFile(logo, "media/rooms/"+filename)
+	if err != nil {
+		http.Error(ctx.Writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	flash.Set("success", "Successfully upgrade the room by ID: "+id)
+	ctx.Redirect(http.StatusFound, "/admin/detail?table_name=rooms&field_id="+id)
 }
